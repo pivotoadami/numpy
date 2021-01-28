d73b7ae87c895c17eb67e3a27013df9c0df7cdf2
pivot <pivotoadami@gmail.com>
2021-01-28 15:24:35 -0300

    Melhorando o código inicial

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-print("Numpy")
+print("Numpy - duas maneiras de criar arrays em python usando numpy")
 
 array1 = np.array([2,4,6])
 
 array2 = np.arange(2, 15, 1)
 
+print(array1)
 print(array2)
